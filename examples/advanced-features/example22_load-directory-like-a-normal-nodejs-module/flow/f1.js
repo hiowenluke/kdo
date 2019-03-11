7a6adde79fe48a091245e293dc1e2cf6e9e48853
Owen Luke <hi.owen.luke@gmail.com>
2019-03-11 18:09:03 +0800

    .

--- examples/advanced-features/example22_load-directory-like-a-normal-nodejs-module/flow/f1.js
+++ examples/advanced-features/example22_load-directory-like-a-normal-nodejs-module/flow/f1.js
@@ -3,7 +3,6 @@
 // we need to access "this" inside the function.
 const fn = async function({a1, a2, a3}) {
 
-	// this.log === this.log, cool!
 	this.log(this.fnName, 'do something');
 	this.log(a1, a2, a3);
 };
