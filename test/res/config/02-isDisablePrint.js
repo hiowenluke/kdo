b4e51b63cd151b7469a9c7b2d4073f1add9bc4b6
Owen Luke <hi.owen.luke@gmail.com>
2019-03-24 13:40:35 +0800

    .

---
+++ test/res/config/02-isDisablePrint.js
@@ -0,0 +1,33 @@
+
+const kdo = require('../../../lib');
+
+let str = '';
+
+const flow = {
+	async f1() {
+		str += this.fnName;
+	},
+
+	async f2() {
+		str += this.fnName;
+	},
+
+	async f3() {
+		str += this.fnName;
+	}
+};
+
+const verify = (value) => {
+	return value === true;
+};
+
+const run = async () => {
+	kdo.config({isDisablePrint: true});
+	await kdo.do(flow);
+
+	const state = kdo.config.getIsDisablePrint();
+	return state;
+};
+
+const info = {verify, run};
+module.exports = info;
