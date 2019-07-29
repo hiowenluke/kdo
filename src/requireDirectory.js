1670753d970c5e09f4cecf274a3fc12472c984a3
Owen Luke <hi.owen.luke@gmail.com>
2019-07-30 02:10:06 +0800

    requireDirectory.js: optimize code

--- src/requireDirectory.js
+++ src/requireDirectory.js
@@ -12,63 +12,67 @@ const fn = (...args) => {
 	// The first argument must be one of the following:
 	//		1. Node.js module object
 	//		2. {filename}
-	//		3. undefined. The module parameter is omitted, so kdo will simulate it
-	//		4. A string. The module parameter is omitted, so kdo will simulate it, and the string will be relative path of it.
+	//		3. options. The module parameter is omitted, so kdo will simulate it.
+	//		4. undefined. The module parameter is omitted, so kdo will simulate it.
+	//		5. A string. The module parameter is omitted, so kdo will simulate it, and the string will be relative path of it.
 	const obj = args[0];
 
-	// 1. Node.js module object //  kdo(module)
-	if (typeof obj === 'object' && obj.constructor.name === 'module') {
-		// do nothing
-	}
+	if (typeof obj === 'object') {
 
-	else
-
-	// 2. {filename}
-	// 		kdo({filename: "/path/to/directory/file.js"}) =>
-	// 		kdo({filename: "/path/to/directory/."})
-	if (typeof obj === 'object' && typeof obj.filename === 'string') {
-		const filename = obj.filename;
-
-		// kdo({filename: "/path/to/directory/."})
-		if (/\/\.$/.test(filename)) {
+		// 1. Node.js module object //  kdo(module)
+		if (obj.constructor.name === 'module') {
 			// do nothing
 		}
 
 		else
 
-		// kdo({filename: "/path/to/directory/"})
-		if (/\/$/.test(filename)) {
-			obj.filename += '.';
-		}
+		// 2. {filename}
+		// 		kdo({filename: "/path/to/directory/file.js"}) =>
+		// 		kdo({filename: "/path/to/directory/."})
+		if (typeof obj.filename === 'string') {
+			const filename = obj.filename;
 
-		else
+			// kdo({filename: "/path/to/directory/."})
+			if (/\/\.$/.test(filename)) {
+				// do nothing
+			}
 
-		// kdo({filename: "/path/to/directory"})
-		if (!/\.js$/.test(filename)) {
-			obj.filename += '/.';
-		}
+			else
 
-		else {
-			// kdo({filename: "/path/to/directory/file.js"})
+			// kdo({filename: "/path/to/directory/"})
+			if (/\/$/.test(filename)) {
+				obj.filename += '.';
+			}
 
-			// "/path/to/directory"
-			const parentPath = path.resolve(filename, '../');
+			else
 
-			// "/path/to/directory/."
-			obj.filename = parentPath + '/.';
+			// kdo({filename: "/path/to/directory"})
+			if (!/\.js$/.test(filename)) {
+				obj.filename += '/.';
+			}
+
+			else {
+				// kdo({filename: "/path/to/directory/file.js"})
+
+				// "/path/to/directory"
+				const parentPath = path.resolve(filename, '../');
+
+				// "/path/to/directory/."
+				obj.filename = parentPath + '/.';
+			}
 		}
 	}
 
 	else
 
-	// 3. The module parameter is omitted // kdo()
+	// 4. The module parameter is omitted // kdo()
 	if (obj === undefined) {
 		simulatedModule.addToArgs(args, caller);
 	}
 
 	else
 
-	// 4. A string relative to the module path // kdo("./tools")
+	// 5. A string relative to the module path // kdo("./tools")
 	if (typeof obj === 'string') {
 		simulatedModule.addToArgs(args, caller);
 	}
