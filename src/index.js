6818522aa02218fbd545617663fa458aa75d98c0
Owen Luke <hi.owen.luke@gmail.com>
2019-07-30 01:30:28 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
 
 const caller = require('caller');
-const lib = require('./__lib');
 
+const requireDirectory = require('./requireDirectory');
 const config = require('./__config');
 const async = require('./async');
 const sync = require('./sync');
 
-const kdoObj = require('./obj');
-
 const kdo = (...args) => {
-
-	// Always load the directory where the caller is located as an object
-	lib.addCallerToArgs(args, caller());
-
-	return kdoObj(...args);
+	return requireDirectory(...args, caller());
 };
 
 Object.assign(kdo, async); // The default is async edition
 kdo.sync = sync; // Attach sync edition
-
 kdo.config = config;
-kdo.obj = kdoObj;
 
 module.exports = kdo;
