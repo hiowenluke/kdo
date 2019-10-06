be6c2cc99280511058116667e1ab8885bab91d35
Owen Luke <hi.owen.luke@gmail.com>
2019-10-07 01:41:43 +0800

    .

---
+++ examples/05-kdo.flow()-or-kdo.dirFn()/01-flow/index.js
@@ -0,0 +1,6 @@
+
+const kdo = require('../../../src');
+
+// kdo.flow() requires the specified directory as a function.
+// kdo.dirFn() is an alias of kdo.flow().
+module.exports = kdo.flow(module);
