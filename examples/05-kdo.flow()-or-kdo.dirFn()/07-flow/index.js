da16edffe6c7451f5ce462cf6518e3a71865ce01
Owen Luke <hi.owen.luke@gmail.com>
2019-10-10 22:33:50 +0800

    .

--- examples/05-kdo.flow()-or-kdo.dirFn()/07-flow/index.js
+++ examples/05-kdo.flow()-or-kdo.dirFn()/07-flow/index.js
@@ -2,9 +2,9 @@
 const kdo = require('../../../src');
 
 const options = {
-	first: 'f3',
-	last: ['f7', 'f4'],
-	exclude: 'f5',
+	first: 'f3', // execute these functions at first
+	last: ['f7', 'f4'], // execute these functions at last
+	exclude: 'f5', // do not execute these functions
 };
 
 module.exports = kdo.flow(options);
